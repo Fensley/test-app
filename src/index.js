e4e6eacec74eae56944cd5e0cc843bb3940d50f3
Fensley <zagalufe@gmail.com>
2024-04-02 15:31:49 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+ ReactDOM.render( 
+<h1 className='Fens'>
+  Fensley
+</h1>
+  ,document.getElementById("root")
+ )
